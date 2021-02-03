30f4c91c1e428e07118f3276f4e0392e88259a8d
WongChoiLam <julian.wcl@outlook.com>
2021-02-03 09:18:53 +0000

    Imporved 1104

--- 1104.py
+++ 1104.py
@@ -1,16 +1,13 @@
-from typing import List
 class Solution:
-    def flip(self, num):
-        first = 1
-        while not first * 2 > num: 
-            first *= 2
+    def flip(self, num, first):
         return first * 2 - 1 - (num - first)
     def pathInZigZagTree(self, label: int) -> List[int]:
         ans = [label]
+        first = 1
+        while not first * 2 > label: 
+            first *= 2
         while label != 1:
-            label = self.flip(int(label / 2))
+            first = int(first / 2)
+            label = self.flip(int(label / 2), first)
             ans.insert(0, label)
         return ans
-        
-s = Solution()
-print(s.pathInZigZagTree(26))
