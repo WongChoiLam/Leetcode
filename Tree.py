1a2a8a0250b75c2b0770be70f50e7b6a8109ee29
WongChoiLam <julian.wcl@outlook.com>
2021-02-05 08:02:28 +0000

    Added Tree

---
+++ Tree.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def listToTree(lst):
+        if len(lst) == 0: return None
+        index = 1
+        root = TreeNode(lst[0])
+        level_node = [root]
+        
+        while index < len(lst):
+            level_index = 0
+            prev = level_node
+            level_node = []
+            while index < len(lst) and level_index < len(prev):
+                if(lst[index] != None):
+                    leftNode = TreeNode(lst[index])
+                    prev[level_index].left = leftNode
+                    level_node.append(leftNode)
+                index += 1
+                if(index < len(lst)):
+                    if(lst[index] != None):
+                        rightNode = TreeNode(lst[index])
+                        prev[level_index].right= rightNode
+                        level_node.append(rightNode)
+                    index += 1
+                level_index += 1
+        return root
